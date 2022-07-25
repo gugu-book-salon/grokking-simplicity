7ed46da1d6b750d83273beac27a020216833c813
chobobdev <chobobdev@naver.com>
2022-07-26 01:18:18 +0900

    refator : refactor given code

--- 1주차/띠비/답자랑/뻐드렁니/answer.js
+++ 1주차/띠비/답자랑/뻐드렁니/answer.js
@@ -4,104 +4,60 @@
 // 반복되는 코드는 삼가주세요
 // 문제 링크 : https://codesandbox.io/s/hamsuhyeongkoding-1juca-ddibi-7widdo?file=/src/index.js:0-3354
 
-const buttons = document.querySelectorAll(".filter-btn");
-const storeItems = document.querySelectorAll(".store-item");
-const sumSpan = document.getElementById("sum");
-const sumTailSpan = document.getElementById("sum-tail");
+main();
 
-let sum = 0;
-[...document.querySelectorAll(".store-item-price")].forEach(
-    (item) => (sum += +item.innerText)
-);
-sumSpan.innerText = `$${sum}`;
-if (sum > 100) {
-    sumTailSpan.innerText = `으로 $100를 넘습니다`;
-} else {
-    sumTailSpan.innerText = `으로 $100를 넘지 못합니다`;
+function setStyleNone(item) {
+  item.style.display = "none";
 }
 
-buttons.forEach(function (button) {
-    button.addEventListener("click", function (e) {
-        sum = 0;
-        const filter = e.target.dataset.filter;
+function setStlyeBlock(item) {
+  return (item.style.display = "block");
+}
 
-        if (filter === "all") {
-            //show all items
-            storeItems.forEach(function (item) {
-                const price = +item.querySelector(".store-item-price").innerText;
-                sum += price;
-                item.style.display = "block";
-            });
-            sumSpan.innerText = `$${sum}`;
-            if (sum > 100) {
-                sumTailSpan.innerText = `으로 $100를 넘습니다`;
-            } else {
-                sumTailSpan.innerText = `으로 $100를 넘지 못합니다`;
-            }
-        } else if (filter === "cakes") {
-            storeItems.forEach(function (item) {
-                if (item.classList.contains("cakes")) {
-                    const price = +item.querySelector(".store-item-price").innerText;
-                    sum += price;
-                    item.style.display = "block";
-                    sumSpan.innerText = `$${sum}`;
-                    if (sum > 100) {
-                        sumTailSpan.innerText = `으로 $100를 넘습니다`;
-                    } else {
-                        sumTailSpan.innerText = `으로 $100를 넘지 못합니다`;
-                    }
-                } else {
-                    item.style.display = "none";
-                }
-            });
-        } else if (filter === "cupcakes") {
-            storeItems.forEach(function (item) {
-                if (item.classList.contains("cupcakes")) {
-                    const price = +item.querySelector(".store-item-price").innerText;
-                    sum += price;
-                    item.style.display = "block";
-                    sumSpan.innerText = `$${sum}`;
-                    if (sum > 100) {
-                        sumTailSpan.innerText = `으로 $100를 넘습니다`;
-                    } else {
-                        sumTailSpan.innerText = `으로 $100를 넘지 못합니다`;
-                    }
-                } else {
-                    item.style.display = "none";
-                }
-            });
-        } else if (filter === "sweets") {
-            storeItems.forEach(function (item) {
-                if (item.classList.contains("sweets")) {
-                    const price = +item.querySelector(".store-item-price").innerText;
-                    sum += price;
-                    item.style.display = "block";
-                    sumSpan.innerText = `$${sum}`;
-                    if (sum > 100) {
-                        sumTailSpan.innerText = `으로 $100를 넘습니다`;
-                    } else {
-                        sumTailSpan.innerText = `으로 $100를 넘지 못합니다`;
-                    }
-                } else {
-                    item.style.display = "none";
-                }
-            });
-        } else if (filter === "doughnuts") {
-            storeItems.forEach(function (item) {
-                if (item.classList.contains("doughnuts")) {
-                    const price = +item.querySelector(".store-item-price").innerText;
-                    sum += price;
-                    item.style.display = "block";
-                    sumSpan.innerText = `$${sum}`;
-                    if (sum > 100) {
-                        sumTailSpan.innerText = `으로 $100를 넘습니다`;
-                    } else {
-                        sumTailSpan.innerText = `으로 $100를 넘지 못합니다`;
-                    }
-                } else {
-                    item.style.display = "none";
-                }
-            });
-        }
+function sumPrice(a, b) {
+  return a + b;
+}
+
+function compareTwo(a, b) {
+  return a > b;
+}
+
+function setSumSpan(sum) {
+  const sumSpan = document.getElementById("sum");
+  sumSpan.innerText = `$${sum}`;
+}
+
+function setSumTailSpan(sum) {
+  const sumTailSpan = document.getElementById("sum-tail");
+  if (compareTwo(sum, 100)) {
+    sumTailSpan.innerText = `으로 $100를 넘습니다`;
+  } else {
+    sumTailSpan.innerText = `으로 $100를 넘지 못합니다`;
+  }
+}
+
+function filterItem(selectedItem) {
+  let sum = 0;
+  const storeItems = document.querySelectorAll(".store-item");
+  storeItems.forEach(function (item) {
+    if (item.classList.contains(selectedItem) || selectedItem === "all") {
+      const price = +item.querySelector(".store-item-price").innerText;
+      sum = sumPrice(sum, price);
+      setStlyeBlock(item);
+      setSumSpan(sum);
+      setSumTailSpan(sum);
+    } else {
+      setStyleNone(item);
+    }
+  });
+}
+
+function main() {
+  const buttons = document.querySelectorAll(".filter-btn");
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      const filter = e.target.dataset.filter;
+      filterItem(filter);
     });
-});
+  });
+}
