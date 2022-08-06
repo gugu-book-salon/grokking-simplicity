1afda5b2bac5758ad27d26ff4dbc5f7818cbcdaf
chobobdev <chobobdev@naver.com>
2022-08-06 17:40:25 +0900

    2차 리뷰 반영

--- 2주차/답자랑/버니/ming.js
+++ 2주차/답자랑/버니/ming.js
@@ -1,60 +1,14 @@
 import "./styles.css";
 
-const assultCookies = [
-  "크런치초코칩 쿠키",
-  "다크카카오 쿠키",
-  "실론나이트 쿠키",
-  "마라맛 쿠키",
-  "라즈베리맛 쿠키",
-  "벨벳케이크맛 쿠키",
-  "구미호맛 쿠키",
-  "자색고구마맛 쿠키",
-  "다크초코 쿠키",
-  "웨어울프맛 쿠키",
-  "근육맛 쿠키",
-  "공주맛 쿠키",
-  "용감한 쿠키"
-];
-const defensiveCookies = [
-  "휘낭시에맛 쿠키",
-  "와일드베리맛 쿠키",
-  "코코아맛 쿠키",
-  "달토끼맛 쿠키",
-  "홀리베리 쿠키",
-  "딸기크레페맛 쿠키",
-  "마들렌맛 쿠키",
-  "우유맛 쿠키",
-  "아보카도맛 쿠키",
-  "용사맛 쿠키",
-  "딸기맛 쿠키"
-];
-const magicianCookies = [
-  "클로티드 크림 쿠키",
-  "서리여왕 쿠키",
-  "펌킨파이맛 쿠키",
-  "오징어먹물맛 쿠키",
-  "망고맛 쿠키",
-  "악마맛 쿠키",
-  "라떼맛 쿠키",
-  "에스프레소맛 쿠키",
-  "감초맛 쿠키",
-  "눈설탕맛 쿠키",
-  "마법사맛 쿠키"
-];
-
 function setAppcontent(cookieList) {
-  let appContent = cookieList.reduce(
-    (acc, current) => acc + `<li>${current}</li>`,
-    ""
-  );
-  return appContent;
+  return cookieList.reduce((acc, current) => acc + `<li>${current}</li>`, "");
 }
 function cookieRemover(contentChildren, filterInput) {
   let filteredCookies = [];
-  [...contentChildren].reduce((acc, current) => {
-    if (!current.textContent.includes(filterInput.value))
-      filteredCookies.push(current.textContent);
-  }, "");
+  [...contentChildren].forEach(function (children) {
+    if (!children.textContent.includes(filterInput.value))
+      filteredCookies.push(children.textContent);
+  });
   return filteredCookies;
 }
 
@@ -75,6 +29,48 @@ function filterBtnListner(content) {
 }
 
 function cookieListSelctor(type) {
+  const assultCookies = [
+    "크런치초코칩 쿠키",
+    "다크카카오 쿠키",
+    "실론나이트 쿠키",
+    "마라맛 쿠키",
+    "라즈베리맛 쿠키",
+    "벨벳케이크맛 쿠키",
+    "구미호맛 쿠키",
+    "자색고구마맛 쿠키",
+    "다크초코 쿠키",
+    "웨어울프맛 쿠키",
+    "근육맛 쿠키",
+    "공주맛 쿠키",
+    "용감한 쿠키"
+  ];
+  const defensiveCookies = [
+    "휘낭시에맛 쿠키",
+    "와일드베리맛 쿠키",
+    "코코아맛 쿠키",
+    "달토끼맛 쿠키",
+    "홀리베리 쿠키",
+    "딸기크레페맛 쿠키",
+    "마들렌맛 쿠키",
+    "우유맛 쿠키",
+    "아보카도맛 쿠키",
+    "용사맛 쿠키",
+    "딸기맛 쿠키"
+  ];
+  const magicianCookies = [
+    "클로티드 크림 쿠키",
+    "서리여왕 쿠키",
+    "펌킨파이맛 쿠키",
+    "오징어먹물맛 쿠키",
+    "망고맛 쿠키",
+    "악마맛 쿠키",
+    "라떼맛 쿠키",
+    "에스프레소맛 쿠키",
+    "감초맛 쿠키",
+    "눈설탕맛 쿠키",
+    "마법사맛 쿠키"
+  ];
+
   const cookieTypeDict = {
     assult: assultCookies,
     defensive: defensiveCookies,
