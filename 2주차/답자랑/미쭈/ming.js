964320718ff2a22952ca3170cbc3431e7e25ceb3
Minju Park <project.mizzu@gmail.com>
2022-08-12 02:30:10 +0900

    week2/mizzu 밍수님 과제 제출
    * refactor : ming.js - 1
    
    * refactor : ming.js - 2
    
    * refactor : ming.js - 3 콘솔로그 제거
    
    * refactor : ming.js appContent 의존성 제거, reduce 활용
    
    * ming.js : 구름님 의견반영(filter로 변경)

--- 2주차/답자랑/미쭈/ming.js
+++ 2주차/답자랑/미쭈/ming.js
@@ -1,127 +1,114 @@
-// https://codesandbox.io/s/hamsuhyeongkoding-2juca-forked-9tyy05?file=/src/index.js
+// https://codesandbox.io/s/dogsyal-2juca-bulbyeonseong-forked-11yych?file=/src/index.js:0-2730
 
-import "./styles.css";
+import './styles.css';
 
-const cookies = [];
 const assultCookies = [
-    "크런치초코칩 쿠키",
-    "다크카카오 쿠키",
-    "실론나이트 쿠키",
-    "마라맛 쿠키",
-    "라즈베리맛 쿠키",
-    "벨벳케이크맛 쿠키",
-    "구미호맛 쿠키",
-    "자색고구마맛 쿠키",
-    "다크초코 쿠키",
-    "웨어울프맛 쿠키",
-    "근육맛 쿠키",
-    "공주맛 쿠키",
-    "용감한 쿠키"
+  '크런치초코칩 쿠키',
+  '다크카카오 쿠키',
+  '실론나이트 쿠키',
+  '마라맛 쿠키',
+  '라즈베리맛 쿠키',
+  '벨벳케이크맛 쿠키',
+  '구미호맛 쿠키',
+  '자색고구마맛 쿠키',
+  '다크초코 쿠키',
+  '웨어울프맛 쿠키',
+  '근육맛 쿠키',
+  '공주맛 쿠키',
+  '용감한 쿠키',
 ];
-const defensiveCookies = [
-    "휘낭시에맛 쿠키",
-    "와일드베리맛 쿠키",
-    "코코아맛 쿠키",
-    "달토끼맛 쿠키",
-    "홀리베리 쿠키",
-    "딸기크레페맛 쿠키",
-    "마들렌맛 쿠키",
-    "우유맛 쿠키",
-    "아보카도맛 쿠키",
-    "용사맛 쿠키",
-    "딸기맛 쿠키"
-];
-const magicianCookies = [
-    "클로티드 크림 쿠키",
-    "서리여왕 쿠키",
-    "펌킨파이맛 쿠키",
-    "오징어먹물맛 쿠키",
-    "망고맛 쿠키",
-    "악마맛 쿠키",
-    "라떼맛 쿠키",
-    "에스프레소맛 쿠키",
-    "감초맛 쿠키",
-    "눈설탕맛 쿠키",
-    "마법사맛 쿠키"
-];
-let filteredCookies = [];
 
-function mergeCookie() {
-    for (let i = 0; i < assultCookies.length; i++) {
-        cookies.push(assultCookies[i]);
-    }
-    for (let i = 0; i < defensiveCookies.length; i++) {
-        cookies.push(defensiveCookies[i]);
-    }
-    for (let i = 0; i < magicianCookies.length; i++) {
-        cookies.push(magicianCookies[i]);
-    }
+const defensiveCookies = [
+  '휘낭시에맛 쿠키',
+  '와일드베리맛 쿠키',
+  '코코아맛 쿠키',
+  '달토끼맛 쿠키',
+  '홀리베리 쿠키',
+  '딸기크레페맛 쿠키',
+  '마들렌맛 쿠키',
+  '우유맛 쿠키',
+  '아보카도맛 쿠키',
+  '용사맛 쿠키',
+  '딸기맛 쿠키',
+];
+
+const magicianCookies = [
+  '클로티드 크림 쿠키',
+  '서리여왕 쿠키',
+  '펌킨파이맛 쿠키',
+  '오징어먹물맛 쿠키',
+  '망고맛 쿠키',
+  '악마맛 쿠키',
+  '라떼맛 쿠키',
+  '에스프레소맛 쿠키',
+  '감초맛 쿠키',
+  '눈설탕맛 쿠키',
+  '마법사맛 쿠키',
+];
+
+main();
+
+function isEqual(a, b) {
+  return a === b;
 }
 
-const content = document.getElementById("content");
-const allButton = document.getElementById("all");
-const assultButton = document.getElementById("assult");
-const defensiveButton = document.getElementById("defensive");
-const magicianButton = document.getElementById("magician");
-const filterInput = document.getElementById("filter-input");
-const filterButton = document.getElementById("filter-button");
+function renderCookieList(content, cookieTypes) {
+  const cookieList = cookieTypes
+    .map((cookie) => `<li>${cookie}</li>`)
+    .reduce((pre, cur) => pre + cur);
+  content.innerHTML = cookieList;
+}
 
-let appContent = "";
-
-allButton.addEventListener("click", () => {
-    mergeCookie();
-    content.innerHTML = "";
-    for (let i = 0; i < cookies.length; i++) {
-        filteredCookies.push(cookies[i]);
-        appContent += `<li>${cookies[i]}</li>`;
+function renderCookieListByMenu(target, content) {
+  const cookieListAll = [
+    ...assultCookies,
+    ...defensiveCookies,
+    ...magicianCookies,
+  ];
+  const menu = ['all', 'assult', 'defensive', 'magician'];
+  const list = [
+    cookieListAll,
+    assultCookies,
+    defensiveCookies,
+    magicianCookies,
+  ];
+  menu.forEach((item, index) => {
+    if (isEqual(target, item)) {
+      renderCookieList(content, list[index]);
     }
-    content.innerHTML = appContent;
-    appContent = "";
-});
+  });
+}
 
-assultButton.addEventListener("click", () => {
-    content.innerHTML = "";
-    for (let i = 0; i < assultCookies.length; i++) {
-        filteredCookies.push(cookies[i]);
-        appContent += `<li>${assultCookies[i]}</li>`;
-    }
-    content.innerHTML = appContent;
-    appContent = "";
-});
+function filteredCookieArray(contentChildren, filterInput) {
+  let cookiesList = [];
+  const filteredCookies = [...contentChildren].filter(
+    (cookie) => !cookie.textContent.includes(filterInput.value)
+  );
+  filteredCookies.forEach((cookie) => cookiesList.push(cookie.innerText));
+  return cookiesList;
+}
 
-defensiveButton.addEventListener("click", () => {
-    content.innerHTML = "";
-    for (let i = 0; i < defensiveCookies.length; i++) {
-        filteredCookies.push(cookies[i]);
-        appContent += `<li>${defensiveCookies[i]}</li>`;
-    }
-    content.innerHTML = appContent;
-    appContent = "";
-});
+function removeSpecifiedCookieName(content) {
+  const filterInput = document.getElementById('filter-input');
+  const contentChildren = content.children;
+  let filteredCookies = filteredCookieArray(contentChildren, filterInput);
+  renderCookieList(content, filteredCookies);
+  filterInput.value = '';
+}
 
-magicianButton.addEventListener("click", () => {
-    content.innerHTML = "";
-    for (let i = 0; i < magicianCookies.length; i++) {
-        appContent += `<li>${magicianCookies[i]}</li>`;
-    }
-    content.innerHTML = appContent;
-    appContent = "";
-});
+function addClickEventButton() {
+  const content = document.getElementById('content');
+  const cookieBtnGroup = document.querySelector('.cookie-btn-group');
+  const filterButton = document.getElementById('filter-button');
+  cookieBtnGroup.addEventListener('click', (event) => {
+    let target = event.target.id;
+    renderCookieListByMenu(target, content);
+  });
+  filterButton.addEventListener('click', () => {
+    removeSpecifiedCookieName(content);
+  });
+}
 
-filterButton.addEventListener("click", () => {
-    const contentChildren = content.children;
-    filteredCookies = [];
-
-    for (let children of contentChildren) {
-        if (!children.textContent.includes(filterInput.value)) {
-            filteredCookies.push(children.textContent);
-        }
-    }
-
-    content.innerHTML = "";
-    for (let i = 0; i < filteredCookies.length; i++) {
-        appContent += `<li>${filteredCookies[i]}</li>`;
-    }
-    content.innerHTML = appContent;
-    filterInput.value = "";
-});
+function main() {
+  addClickEventButton();
+}
